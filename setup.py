2560b29002af4a870af4426a46286eb0c15edd35
Your Name <38316715+riveSunder@users.noreply.github.com>
2024-03-28 13:52:42 -0600

    remove unused dependency on mpi4py

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ setup(name="fracatal",\
         version = "0.0",\
         description = "Fractal boundaries for the persistence of motile pseudorganisms", \
         install_requires = ["numpy==1.24.2",\
-                "matplotlib==3.7.0",\
-                "mpi4py==3.1.5"] \
+                "matplotlib==3.7.0"]\
         )
 
     
