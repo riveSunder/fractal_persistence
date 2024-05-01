6abefd2b402ddfeb7cd28b8752cdaa86eb1601e0
Your Name <38316715+riveSunder@users.noreply.github.com>
2024-05-01 10:43:45 -0600

    add more tools for saving rules, loading pattern and rule to io.py

--- fracatal/functional_jax/io.py
+++ fracatal/functional_jax/io.py
@@ -5,6 +5,9 @@ import numpy.random as npr
 import os
 import json
 
+import skimage
+import skimage.io as sio
+
 
 THIS_FILE_PATH = os.path.abspath(__file__)
 ROOT_DIR = os.path.split(os.path.split(os.path.split(THIS_FILE_PATH)[0])[0])[0]
@@ -17,7 +20,7 @@ def load_rule_dict():
 
   return rule_dict
 
-def load_rule(name="orbium_unicaudatus"):
+def load_rule(name):
 
   rule_dict = load_rule_dict()
 
@@ -32,3 +35,45 @@ def load_rule(name="orbium_unicaudatus"):
     return -1
   
   return rule_result
+
+def save_rule_dict(rule_dict):
+
+  with open(RULE_PATH, "w") as f:
+    json.dump(rule_dict, f)
+  
+
+def add_rule(rule):
+
+  rule_dict = load_rule_dict()
+  rule_name = rule.keys()[0]
+
+  if rule_name in rule_dict.keys():
+    print(f"rule with name {rule_name} already in rules")
+
+    print("rule names: ")
+
+    for my_key in rule_dict.keys():
+      print(f"\t\t {my_key}")
+
+    return -1
+
+  else:
+    rule_dict[rule_name] = rule[rule_name]
+
+  save_rule_dict(rule_dict)
+
+
+def load_pattern_and_rule(name, load_image=False):
+
+  rule = load_rule(name)
+  if rule == -1:
+    print(f"no rule set found for {name}")
+    return -1
+
+  if load_image:
+    pattern = sio.imread(os.path.join(ROOT_DIR, "patterns", f"{name}.png"))
+    pattern = np.array(pattern)[None,None,:,:]
+  else:
+    pattern = np.load(os.path.join(ROOT_DIR, "patterns", f"{name}.npy"))
+
+  return pattern, rule
