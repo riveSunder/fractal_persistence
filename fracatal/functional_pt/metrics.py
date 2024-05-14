478d1364a3ae04bfeaccf0f274ceb8ea2b0ae438
Your Name <38316715+riveSunder@users.noreply.github.com>
2024-05-14 15:47:05 -0600

    use tensor.to(dtype) instead of torch.tensor(tensor, dtype=dtype) in compute_entropy (avoid cloning warning)

--- fracatal/functional_pt/metrics.py
+++ fracatal/functional_pt/metrics.py
@@ -10,7 +10,7 @@ def compute_entropy(subimage):
   Computes Shannon entropy for pixel values in subimage
   """
   
-  subimage = torch.tensor(255*subimage / subimage.max(), dtype=torch.uint8)
+  subimage = (255*subimage / subimage.max()).to(torch.uint8)
 
   eps = 1e-9
   # compute Shannon entropy 
